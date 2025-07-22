304bd9f4a373c095491b06a6afee252e7f32444e
Harkins <tmngoc1108@gmail.com>
2025-07-22 10:48:18 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import { useState, useCallback } from "react";
-import Content from "./Content";
+import { useState, useMemo } from "react";
+// import Content from "./Content";
 
 // 1. memo() => Higher Order Component (HOC) that prevents unnecessary re-renders
 // memo giúp ghi nhớ các component con, tránh việc re-render không cần thiết
@@ -11,18 +11,49 @@ import Content from "./Content";
 // Ví dụ: nếu bạn truyền một hàm vào component con, và hàm đó không thay đổi, thì component con sẽ không re-render lại khi component cha re-render.
 
 // 3. useMemo() => Returns a memoized value
+// useMemo giúp ghi nhớ giá trị tính toán, tránh việc tính toán lại mỗi lần re-render
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [products, setProducts] = useState([]);
 
-  const handleIncrease = useCallback(() => {
-    setCount((prevCount) => prevCount + 1);
-  }, []);
+  const handleSubmit = () => {
+    setProducts((prev) => [...prev, { name, price: +price }]);
+  };
+
+  const totalPrice = useMemo(() => {
+    const result = products.reduce((total, product) => {
+      console.log("calculate total price");
+      return total + product.price;
+    }, 0);
+    return result;
+  }, [products]);
 
   return (
     <div style={{ padding: "10px 32px" }}>
-      <Content onIncrease={handleIncrease} />
-      <h1>Count: {count}</h1>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <input
+        type="number"
+        placeholder="Price"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add</button>
+      <h2>Total price: {totalPrice}</h2>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
