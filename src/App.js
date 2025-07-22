7043202dd0febfbf8d8d3a7ef8a5a642a857d3bd
Harkins <tmngoc1108@gmail.com>
2025-07-22 09:57:08 +0700

    upload code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import './App.css';
-import { useState } from 'react';
-import Content from './Content';
+import "./App.css";
+import { useState } from "react";
+import Content from "./Content";
+
+// 1. memo() => Higher Order Component (HOC) that prevents unnecessary re-renders
+// memo giúp ghi nhớ các component con, tránh việc re-render không cần thiết
+// Ví dụ: nếu component cha re-render, nhưng component con không thay đổi props (dạng text cố định), thì component con sẽ không re-render.
+
+// 2. useCallback() => Returns a memoized callback function
+// 3. useMemo() => Returns a memoized value
 
 function App() {
-  const [show, setShow] = useState(false);
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-  const handleClick = () => {
-    setShow((prevShow) => !prevShow);
-  }
+  const increase = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const increase2 = () => {
+    setCount2((prevCount) => prevCount + 1);
+  };
 
   return (
-    <div style={{ padding: 32 }} className="App">
-      <button onClick={handleClick}>Toggle</button>
-      {show && <Content />}
+    <div style={{ padding: "10px 32px" }}>
+      <Content count={count} count2={count2} />
+      <h1>
+        {count} : {count2}
+      </h1>
+      <button onClick={increase}>Increase 1</button>
+      <button onClick={increase2}>Increase 2</button>
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,58 +1,13 @@
-import { useRef, useState, useEffect } from "react";
+import { memo } from "react";
 
-// useRef lưu các giá trị qua 1 tham chiếu bên ngoài vòng lặp render
-
-function Content() {
-  const [count, setCount] = useState(118);
-
-  // Khi để biến timerRef.current bên trong hàm Content, mỗi lần render lại sẽ gọi lại hàm này và tạo ra một biến mới,
-  // dẫn đến việc không thể dừng timer đúng cách.
-  // Khi log giá trị của timerRef.current, nó sẽ là undefined nếu để bên trong hàm Content.
-  // Nếu để bên ngoài hàm Content, nó sẽ giữ giá trị qua các lần render
-  // và có thể dừng timer đúng cách.
-  // Vì vậy, ta để biến timerRef.current bên ngoài hàm Content.
-
-  // với useRef, giá trị của nó khi khai báo là kiểu Object,
-  // và giá trị của nó sẽ không thay đổi ngoài kiểm soát qua các lần render.
-  // ref.current là giá trị thực tế mà ta muốn lưu trữ.
-
-  const timerRef = useRef();
-  const prevCount = useRef();
-  const h1Ref = useRef();
-
-  useEffect(() => {
-    prevCount.current = count;
-  }, [count]);
-
-  useEffect(() => {
-    // Khi component unmount, dừng timer nếu nó đang chạy
-    console.log(h1Ref.current);
-  });
-
-  const handleStart = () => {
-    // Sử dụng useRef để lưu trữ giá trị của setInterval
-    timerRef.current = setInterval(() => {
-      setCount((prevCount) => prevCount - 1);
-    }, 1000);
-    console.log("Timer started", timerRef.current);
-  };
-
-  const handleStop = () => {
-    // Sử dụng useRef để lưu trữ giá trị của setInterval và dừng nó
-    clearInterval(timerRef.current);
-    console.log("Timer stopped", timerRef.current);
-  };
-
-  console.log(count, prevCount.current);
-
+function Content({ count, count2 }) {
+  console.log("Changed");
   return (
     <div>
-      {/* Cách gán ref kiểu này sẽ giống với getElementById */}
-      <h1 ref={h1Ref}>Count: {count}</h1>
-      <button onClick={handleStart}>Start</button>
-      <button onClick={handleStop}>Stop</button>
+      <h2>Hello World! {count}</h2>
+      <h3>Hello Star!</h3>
     </div>
   );
 }
 
-export default Content;
+export default memo(Content);
